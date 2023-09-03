b5b8eee4a7409cecab872047247aac59b3c7bd43
devseag <noexample@gmail.com>
2023-09-03 11:08:50 -0300

    add restTemplate RestTemplateResponseErrorHandler IdNotFoundException

--- resttemplate/src/main/java/ru/job4j/resttemplate/config/AppConfig.java
+++ resttemplate/src/main/java/ru/job4j/resttemplate/config/AppConfig.java
@@ -4,10 +4,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import ru.job4j.resttemplate.handler.RestTemplateResponseErrorHandler;
+
 @Configuration
 public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+//        return new RestTemplate();
+        return new RestTemplateBuilder()
+                .errorHandler(new RestTemplateResponseErrorHandler())
+                .build();
     }
 }
